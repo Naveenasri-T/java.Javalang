bf8c6026a73dc837af4930e05a9dae359695d20c
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2023-10-29 20:58:07 +0530

    Add files via upload

---
+++ Arthimatic.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class Arthimatic {
+    public static void main(String args[]) {
+        // declare the variables
+        int a = 10, b = 15;
+        // addition operator
+        System.out.println("a+b = " + (a + b)); // 25
+        // subraction operator
+        System.out.println("a-b =" + (a - b)); // -5
+        // multiplication operator
+        System.out.println("a*b =" + (a * b)); // 150
+        // division operator
+        System.out.println("a/b =" + (a / b)); // 0
+        // modulo operator
+        System.out.println("a%b =" + (a % b)); // 10
+
+    }
+}

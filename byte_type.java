4cb7c6cb60618617011a986e95f28e00cf748439
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2023-10-30 19:59:48 +0530

    Add files via upload

---
+++ byte_type.java
@@ -0,0 +1,9 @@
+public class byte_type {
+    public static void main(String args[]) {
+        byte range = 124;
+        int value = 300;
+        System.out.println(range);
+        System.out.println(value);
+
+    }
+}

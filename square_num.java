9cdabd05af18685bc7a8b7f4ec21f6e42bc2815c
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2023-11-11 18:33:40 +0530

    Add files via upload

---
+++ square_num.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class square_num{
+    public static void main(String[] args) {
+        int num,square;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number :");
+        num=sc.nextInt();
+        square=num*num;
+        System.out.println("square of number:"+square);
+
+
+
+
+    }
+
+}

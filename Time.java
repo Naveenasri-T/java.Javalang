cfec26498f866a996fd9e5027c3ed4ef113cf370
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2023-11-11 18:46:01 +0530

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+public class Time {
+    public static void main(String[] args) {
+        LocalDate obj=LocalDate.now();
+        System.out.println("Todays date:"+obj);
+
+    }
+    
+}

37acb0331dd3b4c3c4de5f93ff4a38268ab73350
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2023-11-12 19:13:26 +0530

    Add files via upload

---
+++ mathFunction.java
@@ -0,0 +1,11 @@
+public class mathFunction {
+    public static void main(String[] args) {
+        double x=28;
+        double y=12;
+        System.out.println("maximum of number:"+Math.max(x,y));
+        System.out.println("minmum of number:"+Math.min(x,y));
+        
+
+    }
+    
+}

d9c92339e5da086c9db5bb14d7329ea246c90b72
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2023-11-12 19:13:54 +0530

    Add files via upload

---
+++ breakExample.java
@@ -0,0 +1,14 @@
+public class breakExample {
+    public static void main(String[] args) {
+        int i;
+        for(i=0; i<=100;i++){
+            if (i==50){
+                break;
+            }
+        System.out.println(i);
+            
+
+        }
+    }
+    
+}

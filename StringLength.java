a500405d8c4e8e32b9a66b9de54341345c2e9ba6
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2023-11-13 19:03:47 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,10 @@
+public class StringLength {
+    public static void main(String[] args) {
+        String str1="java";
+        String str2="python";
+        System.out.println("length of string java :"+str1.length());
+        System.out.println("length of string python :"+str2.length());
+
+    }
+    
+}

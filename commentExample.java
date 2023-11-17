29c58965016cc288bf414fea25d4c8deed2b8e0b
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2023-11-17 21:57:10 +0530

    Add files via upload

---
+++ commentExample.java
@@ -0,0 +1,17 @@
+public class commentExample { 
+    public static void main(String[] args) {    
+     int i=10; // i is a variable with value 10  
+    System.out.println(i);  //printing the variable i 
+    // single line commant
+    }    
+     
+
+    
+//Java Multi Line Comment
+/*  
+This   
+is   
+multi line   
+comment  
+*/ 
+}   

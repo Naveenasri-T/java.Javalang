cedea3d7ec28e912b04faf44b50971e688902d8c
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2023-11-22 18:04:44 +0530

    Add files via upload

---
+++ StringToInt.java
@@ -0,0 +1,9 @@
+public class StringToInt { 
+public static void main(String args[]){  
+String s="200";  
+//Converting String into int using Integer.parseInt()  
+int i=Integer.parseInt(s);  
+System.out.println(i+100);  //after convertion itno integer returns 100+200=300
+System.out.println(s+100); //it gives 200+100 200100
+}}  
+

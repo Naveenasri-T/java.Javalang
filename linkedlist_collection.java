deb233022e8c3293281dcf8a172cf80d87a255be
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2024-01-21 15:35:52 +0530

    Add files via upload

---
+++ linkedlist_collection.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class linkedlist_collection {
+    public static void main(String[] args) {
+        LinkedList <String> list=new LinkedList<>();
+        list.add("technology");
+        list.add("AI");
+        list.add("AR & VR");
+        list.add("blockchain");
+        list.add("cloud computing");
+        list.add("Biotechnology");
+        list.add("web developnment");
+        for(String lists:list){
+            System.out.println(lists);
+        }
+        }
+
+    }
+    
+

59c52da114ceb48643f980f4b94cebd4d4d2cd06
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2024-01-22 22:16:06 +0530

    Add files via upload

---
+++ Vector_Collection.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Vector_Collection {
+    public static void main(String[] args) {
+        Vector<String> v =new Vector<>();
+        v.add("technology");
+        v.add("AI");
+        v.add("AR & VR");
+        v.add("blockchain");
+        v.add("cloud computing");
+        v.add("Biotechnology");
+        v.add("web developnment");
+        for(String lists:v){
+            System.out.println(lists);
+
+        
+    }
+}
+    
+}

1fed387a7655bb9ad26820ab66ca34ad4aa16db2
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2024-03-18 21:04:34 +0530

    Add files via upload

---
+++ NumberToStringConversion.java
@@ -0,0 +1,9 @@
+
+    public class NumberToStringConversion {
+        public static void main(String[] args) {
+            int number = 390063;
+            String strNumber = Integer.toString(number);
+            System.out.println("Number as string: " + strNumber);
+        }
+    }
+    
